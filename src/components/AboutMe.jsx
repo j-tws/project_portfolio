b2d57b077e8147cb97f09bbf067e435d8a5b6481
Justin Tan <justintws10@gmail.com>
2022-10-19 17:10:32 +1100

    got basic project container up

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -5,12 +5,12 @@ function AboutMe(){
   return (
   
     <div className="flex flex-col-reverse items-center w-3/4 py-10 mx-auto mt-10 space-y-0 md:space-y-0 md:flex-row border border-red-500">
-      <div className="flex flex-col px-5 w-6/8 ml-8 m-auto md:w-10/12 md:max-w-1/3 border border-red-500">
+      <div className="flex flex-col px-5 w-6/8 m-auto md:w-10/12 md:max-w-1/3 border border-red-500">
         <h2 className="max-w-md py-8 text-4xl font-primary font-bold text-center md:text-5xl text-amber-400 md:text-left">About Me</h2>
 
-        <p className="mb-3 font-secondary text-center text-darkGrayishBlue md:text-left">Hello! I am Justin Tan, a Melbourne based full stack developer recently graduated from General Assembly. Filled with <span className="text-amber-400 font-bold">motivation, ambition and creativity</span>, I aspire to make a positive yet absolute difference to the world, one text at a time! </p>
+        <p className="mb-3 font-secondary text-sm text-center text-darkGrayishBlue md:text-left md:text-base">Hello! I am Justin Tan, a Melbourne based full stack developer recently graduated from General Assembly. Filled with <span className="text-amber-400 font-bold">motivation, ambition and creativity</span>, I aspire to make a positive yet absolute difference to the world, one text at a time! </p>
 
-        <p className="font-secondary text-center text-darkGrayishBlue md:text-left">Having a background in <span className="text-amber-400 font-bold">hospitality and architecture</span>, I am equipped with a unique set of skills and perspectives that I believe will be a great contribution to the tech industry. Both experiences made me realized that I thrive in <span className="text-amber-400 font-bold">a creative, problem solving</span> career. The joy of creating quality, functional products have never been so <span className="text-amber-400 font-bold">fulfilling</span> and I am excited about the various possibilities I can contribute to the world! </p>
+        <p className="font-secondary text-center text-sm text-darkGrayishBlue md:text-left md:text-base">Having a background in <span className="text-amber-400 font-bold">hospitality and architecture</span>, I am equipped with a unique set of skills and perspectives that I believe will be a great contribution to the tech industry. Both experiences made me realized that I thrive in <span className="text-amber-400 font-bold">a creative, problem solving</span> career. The joy of creating quality, functional products have never been so <span className="text-amber-400 font-bold">fulfilling</span> and I am excited about the various possibilities I can contribute to the world! </p>
       </div>
 
       <div className="mx-6 m-auto w-72 md:w-80 border border-red-500">
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,25 +1,59 @@
 import React from "react";
+import { BiLinkExternal } from "react-icons/bi";
+import { FiGithub } from "react-icons/fi";
+import { IconContext } from "react-icons";
 
 function Projects(){
 
   return (
     <div className="mt-10 pt-5 border border-red-500">
-      <h2 className="max-w-md mx-auto text-5xl mb-8 font-primary text-amber-400 font-bold text-center">Projects</h2>
 
+      <h2 className="max-w-md mx-auto text-5xl font-primary text-amber-400 font-bold text-center">Projects</h2>
 
-      <div className="flex flex-row border border-red mx-auto">
-        
-        <div >
-          <img className="object-cover w-150 h-80 border border-red-500" src="img/projects/hipandhop.png" alt="hip_and_hop" />
+      {/* individual project container starts here */}
+      <div className="flex flex-col items-center mx-auto m-8 md:flex-row md:w-4/5 border border-green-500">  
+        <img className="object-cover w-110 h-50 md:object-cover md:w-4/6 md:h-1/2 p-5 border border-red-500 md:m-auto" src="img/projects/hipandhop.png" alt="hip_and_hop" />
+      
+        <div className="w-4/5 border mx-8 mb-8 mx-auto text-center md:p-3 md:mx-auto md:pt-4 md:text-left border-red-500">
+
+          <div className="border border-blue-500">
+            <h3 className="block md:inline-block text-amber-400 text-2xl font-primary mb-3 font-bold">Hip and Hop</h3>
+
+            <IconContext.Provider value={{ color: "#fbbf24b3", className: "global-class-name" }}>
+              <a className="inline-block mx-2" href="https://hipandhop.netlify.app/"><BiLinkExternal/></a>
+              <a className="inline-block mx-1" href="https://github.com/j-tws/hip-and-hop-frontend-1"><FiGithub /></a>
+            </IconContext.Provider>
+          </div>
+
+          <p className="text-stone-300 mb-3 text-sm md:text-base">Hip and Hop is a project consists of two of your most friendly-neighbourhood games in a site. Hop, jump, climb around obstacles and collect items to boost your high scores! If you want to store your high scores into the leadership board, don't forget to sign up and use your jumping skills to topple the leaderboard!</p>
+          <p className="font-primary text-amber-400">Phaser.js &nbsp;  MongoDB &nbsp; Mongoose </p>
+
         </div>
-
-        <div className="w-1/3">
-          <h3>Hip and Hop</h3>
-          <p>Hip and Hop is a project consists of two of your most friendly-neighbourhood games in a site. Hop, jump, climb around obstacles and collect items to boost your high scores! If you want to store your high scores into the leadership board, don't forget to sign up and use your jumping skills to topple the leaderboard!</p>
-          <p>Phaser.js MongoDB Mongoose </p>
-        </div>
-
       </div>
+      {/* individual project container ends here */}
+
+      {/* individual project container starts here */}
+      <div className="flex flex-col items-center mx-auto m-8 md:flex-row md:w-4/5 border border-green-500">  
+        <img className="object-cover w-110 h-50 md:object-cover md:w-4/6 md:h-1/2 p-5 border border-red-500 md:m-auto" src="img/projects/despination.png" alt="hip_and_hop" />
+      
+        <div className="w-4/5 border mx-8 mb-8 mx-auto text-center md:p-3 md:mx-auto md:pt-4 md:text-left border-red-500">
+
+          <div className="border border-blue-500">
+            <h3 className="block md:inline-block text-amber-400 text-2xl font-primary mb-3 font-bold">desPination</h3>
+
+            <IconContext.Provider value={{ color: "#fbbf24b3", className: "global-class-name" }}>
+              <a className="inline-block mx-2" href="https://despination.netlify.app/"><BiLinkExternal/></a>
+              <a className="inline-block mx-1" href="https://github.com/j-tws/hip-and-hop-frontend-1"><FiGithub /></a>
+            </IconContext.Provider>
+          </div>
+
+          <p className="text-stone-300 mb-3 text-sm md:text-base">Inspired by our passion for travelling (which was blocked during the pandemic), desPination is a platform where you can browse various destinations, create your own planners and pin attractions / events into your planner. You can even make notes for your individual planners, before or during your trip!</p>
+          <p className="font-primary text-amber-400">React &nbsp; Rails &nbsp; Node.js &nbsp; React Google Map </p>
+
+        </div>
+      </div>
+      {/* individual project container ends here */}
+
 
       <div>
         <img className="object-cover h-60 w-110 mx-auto" src="img/projects/despination.png" alt="despination" />
